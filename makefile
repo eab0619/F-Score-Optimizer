5f17abfaecefde2f949d716fd5f7fe65a73671c1
Ubuntu <ubuntu@ip-172-31-36-185.ec2.internal>
2022-09-27 19:24:31 +0000

    add data

--- makefile
+++ makefile
@@ -16,3 +16,20 @@ help: makefile
 env: environment.yml
 	@(echo "Creating ${ENV} environment..."; conda env create -f $<) \
 	|| (echo "Updating ${ENV} environment...\n"; conda env update -f $<)
+
+data: data-raw/rawinventor.tsv\
+	data-raw/rawlocation.tsv\
+	data-raw/patent.tsv\
+	data-raw/pv-bipartite.tsv\
+	data-raw/RLdata_bipartite.RData
+	
+data-raw/%.tsv:
+	wget -O $(@F).zip https://s3.amazonaws.com/data.patentsview.org/download/$(@F).zip
+	unzip -o -q $(@F).zip -d $(@D)
+	rm $(@F).zip
+
+data-raw/pv-bipartite.tsv: scripts/make-inventors-bipartite.py
+	conda run -n ${ENV} python3 $<
+
+data-raw/RLdata_bipartite.RData: scripts/make-rldata-bipartite.R
+	Rscript --vanilla $<
--- scripts/make-inventors-bipartite.py
+++ scripts/make-inventors-bipartite.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python3
 
-from curses import raw
 import pandas as pd
-import os
-import shutil
-import wget
-import zipfile
 from pv_evaluation.benchmark import load_patentsview_inventors_benchmark
 
 data = load_patentsview_inventors_benchmark().reset_index()
@@ -18,16 +13,13 @@ dfB["file"] = "B"
 
 combined = pd.concat([dfA, dfB]).reset_index(drop=True)[["mention_id", "unique_id", "file"]]
 
-if not os.path.isfile("rawinventor.tsv.zip"):
-    wget.download("https://s3.amazonaws.com/data.patentsview.org/download/rawinventor.tsv.zip")
-if not os.path.isfile("rawinventor.tsv"):
-    with zipfile.ZipFile("rawinventor.tsv.zip","r") as zip_ref:
-        zip_ref.extractall(".")
+rawinventor = pd.read_csv("data-raw/rawinventor.tsv", sep="\t", dtype=str)
+rawlocation = pd.read_csv("data-raw/rawlocation.tsv", sep="\t", dtype=str)
 
-rawinventor = pd.read_csv("rawinventor.tsv", sep="\t", dtype=str)
 rawinventor["mention_id"] = "US" + rawinventor.patent_id + "-" + rawinventor.sequence
-joined = combined.merge(rawinventor, on="mention_id", how="left")
-joined.to_csv("../data-raw/pv-bipartite.tsv", sep="\t")
-
-os.remove("rawinventor.tsv")
-os.remove("rawinventor.tsv.zip")
+joined = (
+    combined
+    .merge(rawinventor, on="mention_id", how="left")
+    .merge(rawlocation, left_on="rawlocation_id", right_on="id", how="left")
+)
+joined.to_csv("data-raw/pv-bipartite.tsv", sep="\t")

fc7dc1de6f014e62de8e97db033415812124b3c0
Eric Augustus Bai <ericbai@MacBook-Air-2.lan>
2022-10-15 13:19:18 -0400

    updates to the functions to return both metric and dataset

--- scripts/benchmark_metrics.py
+++ scripts/benchmark_metrics.py
@@ -9,17 +9,27 @@ import pandas as pd
 #input: last_name-column name is the last name
 #input: unique_id-column name of the unique id
 
+#output: A list of results, first element is the name variation rate, the second is the original df with name_var
+
 def get_name_var(merged, first_name = 'name_first', last_name = 'name_last', unique_id= 'unique_id'):
     merged_cop = merged.copy()
     merged_cop['name'] = merged[first_name] + ' ' + merged[last_name]
     name_var = merged_cop.groupby([unique_id])['name'].nunique().reset_index(drop=True)
-    return sum(name_var>1)/len(name_var)
+    final_stat =  sum(name_var>1)/len(name_var)
+    
+    cond = merged_cop.groupby([unique_id])['name'].nunique().reset_index().rename(columns={"name": 'var_count'}).assign(is_var=lambda df: (df['var_count'] >1).astype(int))
+    
+    #0 variation count means there is only one name in cluster
+    cond['var_count'] = cond['var_count']-1
+    
+    return [final_stat,merged_cop.merge(cond, on='unique_id', how='left')]
+    
 
 
 ########################################################################################################################
 
 
-##helper function for harmony rate
+##helper function for homonomy rate
 def helper_function(row, merged_cop):
     id_cond = merged_cop.unique_id != row['unique_id']
     name_cond = merged_cop.name == row['name']
@@ -29,23 +39,23 @@ def helper_function(row, merged_cop):
     return 0
 
 
-# Function calculates harmony rate across different clusters
+# Function calculates homonomy rate across different clusters
 
 #input: merged-dataframe with at least first name, last name, and unique id
 #input: first_name-column name of the first name
 #input: last_name-column name is the last name
 #input: unique_id-column name of the unique id
 
-def get_harmony_rate(merged, first_name = 'name_first', last_name = 'name_last', unique_id= 'unique_id'):
+# output: A list of results, first element is the homonomy rate, and the second is the dataframe with homonomy as a column
+
+def get_homonomy_rate(merged, first_name = 'name_first', last_name = 'name_last', unique_id= 'unique_id'):
     merged_cop = merged.copy()
     merged_cop['name'] = merged[first_name] + ' ' + merged[last_name]
     
     #create harmony column
-    merged_cop['harmony'] = merged_cop.apply(helper_function,1, args=(merged_cop,))
+    merged_cop['homonomy'] = merged_cop.apply(helper_function,1, args=(merged_cop,))
    
-        
-    #harmony rate dataset
-    #return merged_cop
     
-    #return actual harmony rate
-    return sum(merged_cop.groupby(['unique_id'])['harmony'].max())/merged_cop['unique_id'].nunique()
+    #return actual homonomy rate
+    return [sum(merged_cop.groupby(['unique_id'])['homonomy'].max())/merged_cop['unique_id'].nunique(),merged_cop]
+

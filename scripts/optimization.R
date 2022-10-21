eb61875d816fbcf56a2832745032e08a64ab5361
Eric <ericctbai@icloud.com>
2022-10-20 20:14:45 -0400

    Fix logic error in iterative step

--- scripts/optimization.R
+++ scripts/optimization.R
@@ -39,8 +39,16 @@ calculate_fn  <- function(posterior, z){
   n1 <- nrow(posterior)-1
   n2 <- ncol(posterior)
   for(j in 1:n2){
-    mid = 1- posterior[z[j],j] - posterior[n1+1,j]
-    fn <- fn + mid
+    if(z[j]!=n1+1){
+      mid = 1- posterior[z[j],j] - posterior[n1+1,j]
+    }
+    else {
+      mid = 1-posterior[n1+1,j]
+    }
+    #
+    #print(mid)
+    fn<- fn+mid
+    #fn <- fn + (z[j]==n1+1)*(1-posterior[n1+1,j])
   }
   fn
 }
@@ -52,8 +60,9 @@ calculate_fp  <- function(posterior, z){
   n1 <- nrow(posterior)-1
   n2 <- ncol(posterior)
   for(j in 1:n2){
-    if(z[j] != n1+1)
+    if(z[j] != n1+1){
       fp <- fp + 1-posterior[z[j],j]
+    }
   }
   fp
 }
@@ -74,18 +83,20 @@ optim_F <- function(posterior, iteration=1, seed=0){
   fn <- 0
   fp <- 0
   
-  
-  for(j in 1:n2){
+
+
     
-    #calculate true positives
-    tp <- calculate_tp(posterior,z)
-    #calculate false negatives; offset since r index starts at 1
-    fn <- calculate_fn(posterior,z)
-    #calculate false positives;  offset since r index starts at 1
-    fp <- calculate_fp(posterior,z)
-  }
+  #calculate true positives
+  tp <- calculate_tp(posterior,z)
+  #calculate false negatives; offset since r index starts at 1
+  fn <- calculate_fn(posterior,z)
+  #calculate false positives;  offset since r index starts at 1
+  fp <- calculate_fp(posterior,z)
+
   f_score <- 2*tp/(2*tp + fp + fn)
   
+  #print(paste0("initial f: ", f_score))
+  #print(paste0("initial fn: ", fn))
   
   for(k in 1:iteration){
     changed = FALSE
@@ -102,7 +113,7 @@ optim_F <- function(posterior, iteration=1, seed=0){
       
       #iterate over matches
       for(i in 1:n1){
-        f_new_match = 2*(tp +posterior[i,j])/( 2*(tp +posterior[i,j]) + fn +fp+posterior[n1+1,j])
+        f_new_match = 2*(tp +posterior[i,j])/( 2*(tp +posterior[i,j]) + fn+(1 - posterior[i,j] -posterior[n1+1,j]) +fp+(1-posterior[i,j]))
         if(f_new_match > f_score){
           changed = TRUE
           if(f_score -f_new_match >f_score_max_change){
@@ -115,7 +126,7 @@ optim_F <- function(posterior, iteration=1, seed=0){
       }
       #after this step i now is the match for j that maximizes the f score
       # check non match
-      f_new_non_match = 2*tp/(2*tp + fn+(1-posterior[n1+1,j]) + fp)
+      f_new_non_match = 2*tp/(2*tp + fn+(1 - posterior[n1+1,j]) + fp)
       if(f_new_non_match > f_score){
         changed = TRUE
         if(f_score -f_new_match >f_score_max_change){
